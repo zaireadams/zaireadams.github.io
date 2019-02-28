c38ff6474829f48863828b41d8dd37b47370a64d
zaireadams <43049471+zaireadams@users.noreply.github.com>
2019-02-28 13:46:35 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ $(document).ready(function() {
     $(".seconds").css("background-color", "rgb(40, 180, " + seconds*4 + ")");
   }, 1000);
 });
+
+
+$(document).ready(function() {
+  setTimeout(function(){
+    $(".alarmimage").addClass("alarm"); 
+    $(".snooze").addClass("alarm");
+  }, 5000);
+
+  $(".button").click(function() { 
+    $(".alarmimage").removeClass("alarm");
+    $(".snooze").removeClass("alarm");
+  });
+
+});
