60a996addec46cd576686abcd0b550ec2c580e77
zaireadams <43049471+zaireadams@users.noreply.github.com>
2019-02-28 18:14:27 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-  setInterval(function() {
+$(document).ready(function()  {
+	setInterval(function() {
     var time = new Date();
     var hour = time.getHours() % 12;
     var minutes = time.getMinutes();
@@ -7,19 +7,18 @@ $(document).ready(function() {
     $(".hours").css("background-color", "rgb(40, 180, " + hour*10 + ")");
     $(".minutes").css("background-color", "rgb(40, 180, " + minutes*4 + ")");
     $(".seconds").css("background-color", "rgb(40, 180, " + seconds*4 + ")");
-  }, 1000);
-});
-
-
-$(document).ready(function() {
+  }, 1000)
   setTimeout(function(){
     $(".alarmimage").addClass("alarm"); 
     $(".snooze").addClass("alarm");
+    $("audio").get(0).play();
   }, 5000);
 
   $(".button").click(function() { 
     $(".alarmimage").removeClass("alarm");
     $(".snooze").removeClass("alarm");
+    $("audio").get(0).pause()
+    $("audio").get(0).currentTime=0;
   });
 
 });
