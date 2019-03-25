0b73c70917dfbfbb348a40fa2c42e207508a73e9
zaireadams <43049471+zaireadams@users.noreply.github.com>
2019-03-25 16:12:51 -0400

    Add files via upload

---
+++ demo/script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var mugen = new MUGEN();
+  mugen.initialize({attackRadius: 100, monsterHitRadius: 100, attackDuration:600, jumpHeight: 300});
+});
