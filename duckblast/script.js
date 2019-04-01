5fd567b9a39de7f906ea37824ccc07dbcd5f7140
zaireadams <43049471+zaireadams@users.noreply.github.com>
2019-04-01 18:23:21 -0400

    Add files via upload

--- duckblast/script.js
+++ duckblast/script.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
   var mugen = new MUGEN();
-  mugen.initialize({attackRadius: 100, monsterHitRadius: 100, attackDuration:800, jumpHeight: 900, herospeed: 300});
+  mugen.initialize({titleScreenImageURL:"icons/horizontalsplash.png" attackRadius: 100, monsterHitRadius: 100, attackDuration:800, jumpHeight: 900, herospeed: 300});
 });
